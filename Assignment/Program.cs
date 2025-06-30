b794d4fd043bdb6e6d53c0e437dfb61dbdc4fc4b
Anas Madkour <anas.mohamed0976@gmail.com>
2025-06-30 20:39:39 +0300

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,10 +6,27 @@
         {
             #region Q1
             //Write a program that allows the user to enter a number then print it
-            Console.Write("Enter a number: ");
-            double number = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"You entered: {number}");
+            //Console.Write("Enter a number: ");
+            //double number = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"You entered: {number}");
             #endregion
+
+            #region Q2
+            string name = "ahmed123";
+
+            // Safe conversion using TryParse
+            if (int.TryParse(name, out int number))
+            {
+                Console.WriteLine($"Converted number: {number}");
+            }
+            else
+            {
+                Console.WriteLine("contains non-numeric characters");
+                #endregion
+            }
         }
+
     }
 }
+    
+
