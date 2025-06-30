879999cc4f931871f1042ceb2e1816f14850145f
Anas Madkour <anas.mohamed0976@gmail.com>
2025-06-30 20:06:51 +0300

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Write a program that allows the user to enter a number then print it
+            Console.Write("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"You entered: {number}");
+            #endregion
         }
     }
 }
