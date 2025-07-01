801df080c81762860330a05743c8f721452161cb
Anas Madkour <anas.mohamed0976@gmail.com>
2025-07-01 16:57:26 +0300

    Q6

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Assignment
+﻿using System;
+using System.ComponentModel;
+
+namespace Assignment
 {
     internal class Program
     {
@@ -6,17 +9,17 @@
         {
 
 
-            #region Q5
-            //Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
-            int original = 50;
-            Console.WriteLine($"Original value: {original}");
+            #region Q6
+            //Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen            int original = 50;
+            Person person1 = new Person();
+            person1.Name = "Anas";
 
-            int copy = original;
-            Console.WriteLine($"before modification: {copy}");
-            copy = 100;
-            Console.WriteLine("After modification:");
-            Console.WriteLine($"Original value: {original}");
-            Console.WriteLine($"Copy value: {copy}");
+            Person person2 = person1;
+
+            person2.Name = "Mohamed";
+
+            Console.WriteLine("person1.Name: " + person1.Name);
+            Console.WriteLine("person2.Name: " + person2.Name);
             #endregion
 
         }
