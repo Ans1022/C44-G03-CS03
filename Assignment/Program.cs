0921e8bb31482a18d4d45570b5e4fdbb9ae5aca7
Anas Madkour <anas.mohamed0976@gmail.com>
2025-07-01 17:22:27 +0300

    Q11

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -9,16 +9,20 @@ namespace Assignment
         {
 
 
-            #region Q10
-            //Write a program that uses the ternary operator to check if the temperature is too hot, too cold, or just good. Assign the result in a variable then display the result. Assume that below 10 degrees is "Just Cold", above 30 degrees is 
-            Console.Write("Enter the degrees: ");
-            int degrees = Convert.ToInt32(Console.ReadLine());
+            #region Q11
+            //Write a program that takes the date from the user and displays it in various formats using string interpolation
+            Console.Write("Enter the day: ");
+            int day = Convert.ToInt32(Console.ReadLine());
 
-            string result = (degrees < 10) ? "Just Cold" :
-                            (degrees > 30) ? "Just Hot"  : "Just Good";
-                           
+            Console.Write("Enter the month: ");
+            int month = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("The weather is: " + result);
+            Console.Write("Enter the year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Today's date : {day} , {month} , {year}");
+            Console.WriteLine($"Today's date : {day} / {month} / {year}");
+            Console.WriteLine($"Today's date : {day} – {month} – {year}");
             #endregion
 
         }
