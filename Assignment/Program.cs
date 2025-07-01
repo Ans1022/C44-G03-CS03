c0e12a55b78ca43350f54be1ca31fdf0ca7ef0fe
Anas Madkour <anas.mohamed0976@gmail.com>
2025-07-01 17:30:53 +0300

    Q12

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -8,26 +8,17 @@ namespace Assignment
         static void Main(string[] args)
         {
 
+            #region Q12
+            // What is the output of the following C# code?
+            DateTime date = new DateTime(2024, 6, 14);
+            Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
 
-            #region Q11
-            //Write a program that takes the date from the user and displays it in various formats using string interpolation
-            Console.Write("Enter the day: ");
-            int day = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the month: ");
-            int month = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the year: ");
-            int year = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine($"Today's date : {day} , {month} , {year}");
-            Console.WriteLine($"Today's date : {day} / {month} / {year}");
-            Console.WriteLine($"Today's date : {day} – {month} – {year}");
+            // output = C
             #endregion
+
+
 
         }
 
     }
 }
-    
-
